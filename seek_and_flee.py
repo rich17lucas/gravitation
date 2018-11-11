a22fbe5234646745b11064c05ebf02e0da21d301
rich17lucas <richard.lucas@caymancomputers.co.uk>
2018-11-11 09:10:36 +0000

    Added command arguments for seek or flee behaviour Uses the sys module and checks if the file is started with seek or flee

--- seek_and_flee.py
+++ seek_and_flee.py
@@ -1,10 +1,11 @@
 import cocos
 import cocos.euclid as eu
 import cocos.particle_systems as ps
+import sys
 
 
 class Actor(cocos.cocosnode.CocosNode):
-    def __init__(self, x, y):
+    def __init__(self, x, y, behaviour):
         super(Actor, self).__init__()
         self.position = (x, y)
         self.velocity = eu.Vector2(0, 0)
@@ -14,6 +15,7 @@ class Actor(cocos.cocosnode.CocosNode):
         self.target = None
         self.add(ps.Sun())
         self.schedule(self.update)
+        self._behaviour = 1 if behaviour == "seek" else -1
 
     def update(self, dt):
         if self.target is None:
@@ -22,7 +24,7 @@ class Actor(cocos.cocosnode.CocosNode):
         steering = distance * self.speed - self.velocity
         steering = self.truncate(steering, self.max_force)
         self.velocity = self.truncate(self.velocity + steering, self.max_velocity)
-        self.position += self.velocity * dt
+        self.position += self.velocity * dt * self._behaviour
 
     @staticmethod
     def truncate(vector, m):
@@ -35,9 +37,9 @@ class Actor(cocos.cocosnode.CocosNode):
 class MainLayer(cocos.layer.Layer):
     is_event_handler = True
 
-    def __init__(self):
+    def __init__(self, behaviour):
         super(MainLayer, self).__init__()
-        self.actor = Actor(320, 240)
+        self.actor = Actor(320, 240, behaviour)
         self.add(self.actor)
 
     def on_mouse_motion(self, x, y, dx, dy):
@@ -45,6 +47,12 @@ class MainLayer(cocos.layer.Layer):
 
 
 if __name__ == '__main__':
-    cocos.director.director.init(caption='Steering Behaviours')
-    scene = cocos.scene.Scene(MainLayer())
-    cocos.director.director.run(scene)
+
+    if len(sys.argv) == 2:
+        cocos.director.director.init(caption='Steering Behaviours')
+        scene = cocos.scene.Scene(MainLayer(sys.argv[1]))
+        cocos.director.director.run(scene)
+    else:
+        print("Usage: seek_and_flee.py seek|flee")
+        sys.exit(99)
+
